7568f1d4f64f0f16146950126c1c9af3c215b3e2
Bukhori Inst <58775021+bukhori-inst@users.noreply.github.com>
2021-06-11 23:36:20 +0700

    Add assets images

--- planet_food/src/views/Home.vue
+++ planet_food/src/views/Home.vue
@@ -3,6 +3,22 @@
     <navbar />
     <div class="container">
       <Hero />
+      <row class="row mt-4">
+        <div class="col">
+          <h2>Best <Strong>Foods</Strong></h2>
+        </div>
+        <div class="col">
+          <router-link to="/foods" class="btn btn-success float-right"
+            ><b-icon-eye></b-icon-eye> See All</router-link
+          >
+        </div>
+      </row>
+
+      <div class="row mb-3">
+        <div class="col-md-4 mt-4">
+          <card-product />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -11,12 +27,14 @@
 // @ is an alias to /src
 import Navbar from '@/components/Navbar.vue';
 import Hero from '@/components/Hero.vue';
+import CardProduct from '@/components/CardProduct.vue';
 
 export default {
   name: 'Home',
   components: {
     Navbar,
     Hero,
+    CardProduct,
   },
 };
 </script>
