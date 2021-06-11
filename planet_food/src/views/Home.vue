e7103a7439ec2e43189a728d73ca9a60e9a38d04
Bukhori Inst <58775021+bukhori-inst@users.noreply.github.com>
2021-06-11 22:05:07 +0700

    Create hero

--- planet_food/src/views/Home.vue
+++ planet_food/src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <navbar/>
-    <h1>Home</h1>
+    <navbar />
+    <div class="container">
+      <Hero />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Navbar from '@/components/Navbar.vue'
+import Navbar from '@/components/Navbar.vue';
+import Hero from '@/components/Hero.vue';
 
 export default {
   name: 'Home',
   components: {
-    Navbar
-  }
-}
+    Navbar,
+    Hero,
+  },
+};
 </script>
